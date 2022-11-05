60e9377a87eebf3ab99d8414796da48e5bfd0c7b
Irish Micole Cando <irishmicolecando@gmail.com>
2022-11-05 10:02:03 +0800

    Initial commit

---
+++ arraymethods.py
@@ -0,0 +1,10 @@
+# Write a python program that do the following:
+
+# - display the initial content of the array
+# - display a menu of options
+# - allow user to select item in the menu (check if valid)
+# - perform the selected option (you may prompt additional info to user when need)
+# - display the resulting values of the array
+
+# Note: 
+# - The program has an array variable containing 10 random numbers
